239b717423331f5d3cb97c54fa3bed167e311092
nhattranb <s3926629@rmit.edu.vn>
2023-01-22 12:07:52 +0700

    The project load

---
+++ src/question1/Animal.java
@@ -0,0 +1,18 @@
+/*
+  RMIT University Vietnam
+  Course: COSC2081 - Programming 1
+  Semester: 2022C
+  Assessment: Test 2 (Final Online Exam)
+  Author: Tran Minh Nhat
+  ID: s3926629
+  JDK version: Amazon Corretto 11
+  Created date: 14/01/2023
+  Acknowledgement: 1.
+*/
+
+
+package question1;
+
+public class Shape {
+    private 
+}
